ddbe97c25597edfa56690a68e5cc7de6d84dfa6b
Ross Fish <ross@4mscompany.com>
2025-06-30 10:16:55 -0700

    DJFilter: updated knob widgets

--- src/DJFilter.cpp
+++ src/DJFilter.cpp
@@ -167,9 +167,9 @@ struct DJFilterWidget : ModuleWidget {
 		addChild(createWidget<ScrewSilver>(Vec(RACK_GRID_WIDTH, RACK_GRID_HEIGHT - RACK_GRID_WIDTH)));
 		addChild(createWidget<ScrewSilver>(Vec(box.size.x - 2 * RACK_GRID_WIDTH, RACK_GRID_HEIGHT - RACK_GRID_WIDTH)));
 
-		addParam(createParamCentered<RoundBlackKnob>(mm2px(Vec(15.24, 18.803)), module, DJFilter::CUTOFF_PARAM));
-		addParam(createParamCentered<RoundBlackKnob>(mm2px(Vec(15.24, 41.501)), module, DJFilter::RESONANCE_PARAM));
-		addParam(createParamCentered<RoundBlackKnob>(mm2px(Vec(15.24, 63.747)), module, DJFilter::SLOPE_PARAM));
+		addParam(createParamCentered<Davies1900hBlackKnob>(mm2px(Vec(15.24, 18.803)), module, DJFilter::CUTOFF_PARAM));
+		addParam(createParamCentered<Davies1900hBlackKnob>(mm2px(Vec(15.24, 41.501)), module, DJFilter::RESONANCE_PARAM));
+		addParam(createParamCentered<Davies1900hBlackKnob>(mm2px(Vec(15.24, 63.747)), module, DJFilter::SLOPE_PARAM));
 
 		addInput(createInputCentered<PJ301MPort>(mm2px(Vec(7.303, 82.991)), module, DJFilter::CUTOFFCV_INPUT));
 		addInput(createInputCentered<PJ301MPort>(mm2px(Vec(23.001, 83.001)), module, DJFilter::RESONANCECV_INPUT));
