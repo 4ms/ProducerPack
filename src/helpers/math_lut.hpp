49fc2bd3499ae05faf7478bb003c937eae74bfd1
Ross Fish <ross@4mscompany.com>
2025-10-23 13:17:03 -0700

    Math_lut.hpp: defined lookup tables

--- src/helpers/math_lut.hpp
+++ src/helpers/math_lut.hpp
@@ -1,3 +1,8 @@
 #include "lut.hh"
 
 extern const Mapping::LookupTable_t<64, float> Sin;
+extern const Mapping::LookupTable_t<64, float> Cos;
+extern const Mapping::LookupTable_t<64, float> Exp;
+extern const Mapping::LookupTable_t<64, float> Log;
+
+
